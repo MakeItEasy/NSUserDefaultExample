c1f45c2679c11d36669f6e078387b0bf401f25e9
MakeItEasy <rugang6891@gmail.com>
2015-02-09 10:23:48 +0800

    use nsuserdefault to save data

--- NSUserDefaultExample/ViewController.swift
+++ NSUserDefaultExample/ViewController.swift
@@ -6,13 +6,20 @@
 //  Copyright (c) 2015年 moyan. All rights reserved.
 //
 
+// http://code.tutsplus.com/tutorials/ios-sdk-working-with-nsuserdefaults--mobile-6039
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtAge: UITextField!
+    @IBOutlet weak var imgAvatar: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadUserSettings()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imgPickerTapped(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(picker, animated: true, completion: nil)
+        
+    }
 
+    @IBAction func btnSaveTapped(sender: AnyObject) {
+        // Hide the keyboard
+        txtUsername.resignFirstResponder()
+        txtPassword.resignFirstResponder()
+        txtAge.resignFirstResponder()
+        
+        let imageData = UIImageJPEGRepresentation(imgAvatar.image, 100)
+        
+        let userDefault = NSUserDefaults.standardUserDefaults()
+        userDefault.setValue(txtUsername.text, forKey: "username")
+        userDefault.setValue(txtPassword.text, forKey: "password")
+        userDefault.setValue(txtAge.text.toInt(), forKey: "age")
+        userDefault.setObject(imageData, forKey: "avatar")
+        userDefault.synchronize()
+        NSLog("Data saved")
+    }
+    
+    // # Mark
+    // Picker Controller Delegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        self.imgAvatar.image = info[UIImagePickerControllerEditedImage] as UIImage
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // private
+    private func loadUserSettings() {
+        let userDefault = NSUserDefaults.standardUserDefaults()
+        txtUsername.text = userDefault.stringForKey("username")
+        txtPassword.text = userDefault.stringForKey("password")
+        txtAge.text = userDefault.integerForKey("age").description
+        let imageData = userDefault.dataForKey("avatar")
+        if imageData != nil {
+            imgAvatar.image = UIImage(data: imageData!)
+        }
+    }
 }
 
